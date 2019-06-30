2600850d6dc04a12ed397686bffd7735162c157b
mohammad <chatcart3@gmail.com>
2019-06-30 21:49:16 +0430

    a

--- app/src/main/java/me/live/livebottom/MainActivity.kt
+++ app/src/main/java/me/live/livebottom/MainActivity.kt
@@ -11,12 +11,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val arrayList = arrayListOf(0,1,2)
+        var i =1
         LiveBottomNav.setOnNavigationItemChangedListener(object : OnLiveNavigationItemChangeListener {
             override fun onLiveNavigationItemChanged(liveNavigation: LiveBottomnavigation.LiveNavigation) {
-
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
         })
     }
+
+
+
 }
 
 
